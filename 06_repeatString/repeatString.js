b000715c07587b7ff3c5f05174940af0c311c6a0
Yared <penayared16@gmail.com>
2025-07-13 12:55:08 -0400

    06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,47 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    /* 
+        CREATE a variable to store the result (repeated string)
+
+        IF the num of repeats to be done is negative 
+            RETURN the string 'ERROR'.
+
+        OTHERWISE
+            LOOP up to the num of repeats
+                CONCATENATE the string on each repeat
+        
+            RETURN the result.
+    */
+   
+    let repeat = "";
+
+   if (num < 0 ) {
+        return 'ERROR';
+
+   } else {
+        for (let i = 0; i < num; i++) {
+            repeat = repeat + string;
+        }
+        return repeat;
+   }
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+/*
+string = hey
+num = 3
+"" --> repeat line 2
+
+iter1 = result = hey --> string
+iter2 = result = heyhey --> repeat.concat(string) @ i = 2
+iter3 = result = heyheyhey
+
+inside loop;
+console.log(result);
+hey
+heyhey
+heyheyhey
+
+ */
