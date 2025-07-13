18ea0b988d8be7a754886c13e14f8c19363bd8a4
Yared <penayared16@gmail.com>
2025-07-13 17:08:40 -0400

    09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,40 @@
 const sumAll = function(num1, num2) {
     result = 0;
 
+    function isNumber() {
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+            return 'ERROR';
+        }
+    };
+    
+    function isInteger() {
+        if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+            return 'ERROR';
+        }
+    };
+
+    function isNegative() {
+        if (num1 < 0 || num2 < 0) {
+            return 'ERROR';
+        }
+    };
+
+    if (isNumber() || isInteger() || isNegative()) {
+        return 'ERROR';
+
+    } else if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) {
+                result += i;
+        };
+        return result;
+        
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            
+           result += i; 
+        };
+        return result; 
+    }
 };
 
 // Do not edit below this line
