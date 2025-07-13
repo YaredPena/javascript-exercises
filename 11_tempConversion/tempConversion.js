50f6640a3af5dcd70691f3de8496b8e63a46e2f5
Yared <penayared16@gmail.com>
2025-07-13 19:07:20 -0400

    11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const toCelsiusFormula = (temp - 32) * 5/9;
+  let result = Math.round(toCelsiusFormula * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const toFarenheitFormula = (temp * 9/5) + 32;
+  let result = Math.round(toFarenheitFormula * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
